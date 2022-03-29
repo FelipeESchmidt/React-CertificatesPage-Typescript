ed13d76ce924321322d6f694b0d1473bd3ba5cbc
FelipeESchmidt <schmidtfe2@gmail.com>
2022-03-29 15:44:08 -0300

    bettering switch component

--- src/Components/Switch/index.styles.ts
+++ src/Components/Switch/index.styles.ts
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import { Container } from "../../Styles/CommomStyles";
+import styled from 'styled-components';
+import { Container } from '../../Styles/CommomStyles';
 
 export const StyledWrapper = styled.div`
   width: 100%;
@@ -10,26 +10,25 @@ export const StyledWrapper = styled.div`
 export const StyledContainer = styled(Container)`
   display: flex;
   justify-content: flex-end;
+  cursor: pointer;
 `;
 
 export const StyledSwitch = styled.div`
   position: relative;
   width: 32px;
   height: 14px;
-  background-color: ${({ marked, theme }: any) =>
-    marked ? theme.markedBack : theme.unMarkedBack};
+  background-color: ${({ marked, theme }: any) => (marked ? theme.markedBack : theme.unMarkedBack)};
   border-radius: 20px;
 `;
 
 export const StyledMarker = styled.span`
   position: absolute;
   top: -3px;
-  ${({ marked }: any) => (marked ? "right: -3px" : "left: -3px")};
+  ${({ marked }: any) => (marked ? 'right: -3px' : 'left: -3px')};
   width: 20px;
   height: 20px;
   border-radius: 50%;
-  background-color: ${({ marked, theme }: any) =>
-    marked ? theme.marked : theme.unMarked};
+  background-color: ${({ marked, theme }: any) => (marked ? theme.marked : theme.unMarked)};
   cursor: pointer;
   :hover {
     box-shadow: 0 0 0px 5px rgba(120, 120, 120, 0.15);
--- src/Components/Switch/index.tsx
+++ src/Components/Switch/index.tsx
@@ -1,27 +1,26 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { changeThemeMode } from "../../Redux/App/App.actions";
-import { appSelector } from "../../Redux/App/App.selectors";
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeThemeMode } from '../../Redux/App/App.actions';
+import { appSelector } from '../../Redux/App/App.selectors';
 
-import * as S from "./index.styles";
+import * as S from './index.styles';
 
 function Switch() {
   const dispatch = useDispatch();
   const { mode } = useSelector(appSelector);
 
-  const handleChange = () => {
+  const handleChange = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
     dispatch(changeThemeMode());
   };
 
+  const isMarked = (): boolean => mode === 'dark';
+
   return (
     <S.StyledWrapper>
-      <S.StyledContainer>
-        <S.StyledSwitch {...{ marked: mode === "dark" }}>
-          <S.StyledMarker
-            id="switch-mode"
-            onClick={handleChange}
-            {...{ marked: mode === "dark" }}
-          />
+      <S.StyledContainer onClick={handleChange}>
+        <S.StyledSwitch {...{ marked: isMarked() }}>
+          <S.StyledMarker id="switch-mode" {...{ marked: isMarked() }} />
         </S.StyledSwitch>
       </S.StyledContainer>
     </S.StyledWrapper>
