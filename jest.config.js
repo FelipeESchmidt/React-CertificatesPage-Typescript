27692a9880deb6b49991c20a7edc9100f7e465b1
FelipeESchmidt <schmidtfe2@gmail.com>
2022-03-31 10:54:15 -0300

    adding directory Utils to the test files

--- jest.config.js
+++ jest.config.js
@@ -2,10 +2,16 @@ module.exports = {
   testPathIgnorePatterns: ['<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
   testEnvironment: 'jsdom',
-  roots: ['<rootDir>/src/Components', '<rootDir>/src/Screens', '<rootDir>/src/Redux'],
+  roots: [
+    '<rootDir>/src/Components',
+    '<rootDir>/src/Screens',
+    '<rootDir>/src/Redux',
+    '<rootDir>/src/Utils',
+  ],
   collectCoverageFrom: [
     '<rootDir>/src/Components/**/*.tsx',
     '<rootDir>/src/Screens/**/*.tsx',
     '<rootDir>/src/Redux/**/*.tsx',
+    '<rootDir>/src/Utils/**/*.ts',
   ],
 };
