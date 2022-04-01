759f75c387dfa25ad81f22b973ec4425b5b9031c
FelipeESchmidt <schmidtfe2@gmail.com>
2022-04-01 14:27:33 -0300

    bettering HomeScreen tests

--- src/Screens/Home/Home.integration.test.tsx
+++ src/Screens/Home/Home.integration.test.tsx
@@ -1,16 +1,40 @@
-import { render, screen } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { store } from "../../Redux/store";
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { makeServer } from '../../miragejs/server';
+import { name } from '../../Components/Footer/index.constants';
+import { store } from '../../Redux/store';
 
-import HomeScreen from "./index";
+import HomeScreen from './index';
 
-test("should render Title", () => {
-  render(
-    <Provider store={store}>
-      <HomeScreen />
-    </Provider>
-  );
+describe('HomeScreen > Integration', () => {
+  let server: any;
 
-  const bodyElement = screen.queryByText(/Minhas/i);
-  expect(bodyElement).toBeInTheDocument();
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' });
+    server.timing = 0;
+  });
+
+  afterEach(() => {
+    server.shutdown();
+  });
+
+  const renderComponent = () => {
+    render(
+      <Provider store={store}>
+        <HomeScreen />
+      </Provider>,
+    );
+  };
+
+  it('should render the header', () => {
+    renderComponent();
+
+    expect(screen.getByText(/Minhas/i)).toBeInTheDocument();
+  });
+
+  it('should render the footer', () => {
+    renderComponent();
+
+    expect(screen.getByText(name)).toBeInTheDocument();
+  });
 });
