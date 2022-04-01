761639ddabac650f9f093decfd101a6839f1ad9e
FelipeESchmidt <schmidtfe2@gmail.com>
2022-04-01 16:23:18 -0300

    bettering Switch test to achieve 100% coverage in Reducer

--- src/Components/Switch/Switch.unit.test.tsx
+++ src/Components/Switch/Switch.unit.test.tsx
@@ -1,39 +1,41 @@
-import { Provider } from "react-redux";
-import { store } from "../../Redux/store";
+import { Provider } from 'react-redux';
+import { store } from '../../Redux/store';
 
-import "jest-styled-components";
-import { mount } from "enzyme";
+import 'jest-styled-components';
+import { mount } from 'enzyme';
 
-import Switch from "./index";
-import { ThemeModes } from "../../Theme/index.theme";
+import Switch from './index';
+import { ThemeModes } from '../../Theme/index.theme';
 
-const darkMode: ThemeModes = "dark";
-const lightMode: ThemeModes = "light";
+const darkMode: ThemeModes = 'dark';
+const lightMode: ThemeModes = 'light';
 
-describe("Switch", () => {
+describe('Switch', () => {
   let wrapper;
 
-  it("should render Switch", () => {
+  it('should render Switch', () => {
     wrapper = mount(
       <Provider store={store}>
         <Switch />
-      </Provider>
+      </Provider>,
     );
 
-    const switchElement = wrapper.find("#switch-mode");
+    const switchElement = wrapper.find('#switch-mode');
 
     expect(!!switchElement).toBe(true);
   });
 
-  it("should set Redux value after clicking", () => {
+  it('should set Redux value after clicking', () => {
     wrapper = mount(
       <Provider store={store}>
         <Switch />
-      </Provider>
+      </Provider>,
     );
 
     expect(store.getState().app.mode).toBe(darkMode);
-    wrapper.find("#switch-mode").first().simulate("click");
+    wrapper.find('#switch-mode').first().simulate('click');
     expect(store.getState().app.mode).toBe(lightMode);
+    wrapper.find('#switch-mode').first().simulate('click');
+    expect(store.getState().app.mode).toBe(darkMode);
   });
 });
