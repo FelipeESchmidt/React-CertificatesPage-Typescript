0035910a5324de706566695c3ae21780ddfb8067
FelipeESchmidt <schmidtfe2@gmail.com>
2022-04-01 15:50:48 -0300

    adding allCertificates to the AppTypes and adding CertificateProps as object in arrays

--- src/Redux/App/App.types.ts
+++ src/Redux/App/App.types.ts
@@ -1,12 +1,14 @@
-import { ThemeModes } from "../../Theme/index.theme";
+import { CertificateProps } from '../../Components/Certificate/index.types';
+import { ThemeModes } from '../../Theme/index.theme';
 
-export const SET_SEARCH = "@app/SET_SEARCH";
-export const CHANGE_MODE = "@app/CHANGE_MODE";
-export const CERTIFICATES_LOAD = "@app/CERTIFICATES_LOAD";
-export const CERTIFICATES_LOADED = "@app/CERTIFICATES_LOADED";
+export const SET_SEARCH = '@app/SET_SEARCH';
+export const CHANGE_MODE = '@app/CHANGE_MODE';
+export const CERTIFICATES_LOAD = '@app/CERTIFICATES_LOAD';
+export const CERTIFICATES_LOADED = '@app/CERTIFICATES_LOADED';
 
 export interface AppTypes {
-  certificates: Array<any>;
+  allCertificates: Array<CertificateProps>;
+  certificates: Array<CertificateProps>;
   loading: boolean;
   search: string;
   mode: ThemeModes;
