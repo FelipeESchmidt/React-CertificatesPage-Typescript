0a6b99afb1079c18c80f5e6083402a30cb993576
FelipeESchmidt <schmidtfe2@gmail.com>
2022-04-01 16:39:21 -0300

    developing test to cover input search on app

--- src/Components/Certificates/Certificates.unit.test.tsx
+++ src/Components/Certificates/Certificates.unit.test.tsx
@@ -2,7 +2,9 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { makeServer } from '../../miragejs/server';
+import { setSearch } from '../../Redux/App/App.actions';
 import { store } from '../../Redux/store';
+import { CertificateProps } from '../Certificate/index.types';
 
 import Certificates from './index';
 import { noCertificatesTitle } from './index.constants';
@@ -61,6 +63,30 @@ describe('Certificates > Unit', () => {
     setTimeout(() => {
       expect(screen.queryAllByTestId('certificate')).toHaveLength(0);
       expect(screen.getByText(noCertificatesTitle)).toBeInTheDocument();
+      done();
+    }, 10);
+  });
+
+  fit('should filter certificates after typing term on input', (done) => {
+    const stack = 'React with Redux';
+    server.createList('certificate', 9);
+    let certificate: CertificateProps = server.create('certificate');
+    certificate.info.stacks = [stack];
+
+    render(
+      <Provider store={store}>
+        <Router>
+          <Certificates />
+        </Router>
+      </Provider>,
+    );
+
+    setTimeout(() => {
+      expect(screen.queryAllByTestId('certificate')).toHaveLength(10);
+      store.dispatch(setSearch(stack));
+      expect(screen.queryAllByTestId('certificate')).toHaveLength(1);
+      store.dispatch(setSearch(''));
+      expect(screen.queryAllByTestId('certificate')).toHaveLength(10);
       done();
     }, 10);
   });
